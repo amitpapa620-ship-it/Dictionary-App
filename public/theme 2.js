9903ba9d47429234124a01ab2dc4dce78a00c8dc
Amit Kumar <amitmac369@Amits-MacBook-Air.local>
2026-01-07 19:52:52 +0530

    change in switching night mode

--- public/theme 2.js
+++ public/theme 2.js
@@ -14,10 +14,10 @@
                 // If it's before 6 AM or after 6 PM, apply night mode
                 if (hour < 6 || hour >= 18) {
                     body.classList.add('night-mode');
-                    
+                    console.log("Night mode applied based on time.");
                 } else {
                     body.classList.remove('night-mode');
-                    
+                    console.log("Day mode applied based on time.");
                 }
         }
 
